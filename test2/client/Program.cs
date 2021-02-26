1e80d9460ec5332a1071573eaae9507e6d88a026
Supipi96 <76469306+Supipi96@users.noreply.github.com>
2021-02-26 15:48:26 +0530

    Update Program.cs

--- test2/client/Program.cs
+++ test2/client/Program.cs
@@ -244,7 +244,7 @@ namespace client
         static byte[] Encrypt(byte[] input, RSAParameters publicKey)
         {
             byte[] encrypted;
-            using (var rsa = new RSACryptoServiceProvider(20480))
+            using (var rsa = new RSACryptoServiceProvider(204800))
             {
                 rsa.PersistKeyInCsp = false;
                 rsa.ImportParameters(publicKey);
@@ -269,7 +269,7 @@ namespace client
         {
             Console.WriteLine(input.Length);
             byte[] decrypted;
-            using (var rsa = new RSACryptoServiceProvider(20480))
+            using (var rsa = new RSACryptoServiceProvider(204800))
             {
                 rsa.PersistKeyInCsp = false;
                 rsa.ImportParameters(privateKey);
